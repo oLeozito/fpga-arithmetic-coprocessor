46210142b8a62a5a140ec3ff1663dc6a630c28fb
oLeozito <143969738+oLeozito@users.noreply.github.com>
2025-03-28 17:18:05 -0300

    Add files via upload

--- ramtest.v
+++ ramtest.v
@@ -1,13 +1,40 @@
 module ramtest(
 input clock,
 input sw0,
-output [7:0] barra);
+output reg [7:0] barra);
 
-ram(
+wire [15:0] q1,q2,q3,q;
+reg [7:0] a, b;
+wire [8:0] resultado;
+
+rammaior ram1(
 0,
 clock,
-10,
+5,
 sw0,
-barra
+q1
 );
+
+rammaior ram2(
+1,
+clock,
+2,
+sw0,
+q2
+);
+
+modulo_somador_subtrator(q1, q2, 0, resultado);
+
+rammaior ram3(
+0,
+clock,
+resultado,
+sw0,
+q3
+);
+
+always@(posedge clock) begin
+	barra <= q3[7:0];
+end
+
 endmodule
