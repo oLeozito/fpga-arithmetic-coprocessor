d85099378ac1c358473aa1411f37c651059bd6e3
oLeozito <143969738+oLeozito@users.noreply.github.com>
2025-04-07 14:41:36 -0300

    Soma, Subtração e Multiplicação funcionando.

--- unidade_logica.v
+++ unidade_logica.v
@@ -9,8 +9,6 @@ module unidade_logica(
 );
 
 	reg done_det;
-	reg [1:0] dCount;
-	reg [1:0] delay;
 	
 	initial begin
 		done_det = 1;
@@ -395,6 +393,157 @@ module unidade_logica(
 		 .resultado(matriz_sub[224:216])
 	);
 
+	
+	// Multiplicação
+	modulo_multiplicador inst_multi1(
+     .a(matriz_A[7:0]),
+     .b(matriz_B[7:0]),
+     .resultado(matriz_multi[8:0])
+	);
+
+	modulo_multiplicador inst_multi2(
+     .a(matriz_A[16:9]),
+     .b(matriz_B[16:9]),
+     .resultado(matriz_multi[17:9])
+	);
+
+	modulo_multiplicador inst_multi3(
+     .a(matriz_A[25:18]),
+     .b(matriz_B[25:18]),
+     .resultado(matriz_multi[26:18])
+	);
+
+	modulo_multiplicador inst_multi4(
+     .a(matriz_A[34:27]),
+     .b(matriz_B[34:27]),
+     .resultado(matriz_multi[35:27])
+	);
+
+	modulo_multiplicador inst_multi5(
+     .a(matriz_A[43:36]),
+     .b(matriz_B[43:36]),
+     .resultado(matriz_multi[44:36])
+	);
+
+	modulo_multiplicador inst_multi6(
+     .a(matriz_A[52:45]),
+     .b(matriz_B[52:45]),
+     .resultado(matriz_multi[53:45])
+	);
+
+	modulo_multiplicador inst_multi7(
+     .a(matriz_A[61:54]),
+     .b(matriz_B[61:54]),
+     .resultado(matriz_multi[62:54])
+	);
+
+	modulo_multiplicador inst_multi8(
+     .a(matriz_A[70:63]),
+     .b(matriz_B[70:63]),
+     .resultado(matriz_multi[71:63])
+	);
+
+	modulo_multiplicador inst_multi9(
+     .a(matriz_A[79:72]),
+     .b(matriz_B[79:72]),
+     .resultado(matriz_multi[80:72])
+	);
+
+	modulo_multiplicador inst_multi10(
+     .a(matriz_A[88:81]),
+     .b(matriz_B[88:81]),
+     .resultado(matriz_multi[89:81])
+	);
+
+	modulo_multiplicador inst_multi11(
+     .a(matriz_A[97:90]),
+     .b(matriz_B[97:90]),
+     .resultado(matriz_multi[98:90])
+	);
+
+	modulo_multiplicador inst_multi12(
+     .a(matriz_A[106:99]),
+     .b(matriz_B[106:99]),
+     .resultado(matriz_multi[107:99])
+	);
+
+	modulo_multiplicador inst_multi13(
+     .a(matriz_A[115:108]),
+     .b(matriz_B[115:108]),
+     .resultado(matriz_multi[116:108])
+	);
+
+	modulo_multiplicador inst_multi14(
+     .a(matriz_A[124:117]),
+     .b(matriz_B[124:117]),
+     .resultado(matriz_multi[125:117])
+	);
+
+	modulo_multiplicador inst_multi15(
+     .a(matriz_A[133:126]),
+     .b(matriz_B[133:126]),
+     .resultado(matriz_multi[134:126])
+	);
+
+	modulo_multiplicador inst_multi16(
+     .a(matriz_A[142:135]),
+     .b(matriz_B[142:135]),
+     .resultado(matriz_multi[143:135])
+	);
+
+	modulo_multiplicador inst_multi17(
+     .a(matriz_A[151:144]),
+     .b(matriz_B[151:144]),
+     .resultado(matriz_multi[152:144])
+	);
+
+	modulo_multiplicador inst_multi18(
+     .a(matriz_A[160:153]),
+     .b(matriz_B[160:153]),
+     .resultado(matriz_multi[161:153])
+	);
+
+	modulo_multiplicador inst_multi19(
+     .a(matriz_A[169:162]),
+     .b(matriz_B[169:162]),
+     .resultado(matriz_multi[170:162])
+	);
+
+	modulo_multiplicador inst_multi20(
+     .a(matriz_A[178:171]),
+     .b(matriz_B[178:171]),
+     .resultado(matriz_multi[179:171])
+	);
+
+	modulo_multiplicador inst_multi21(
+     .a(matriz_A[187:180]),
+     .b(matriz_B[187:180]),
+     .resultado(matriz_multi[188:180])
+	);
+
+	modulo_multiplicador inst_multi22(
+     .a(matriz_A[196:189]),
+     .b(matriz_B[196:189]),
+     .resultado(matriz_multi[197:189])
+	);
+	
+	modulo_multiplicador inst_multi23(
+     .a(matriz_A[205:198]),
+     .b(matriz_B[205:198]),
+     .resultado(matriz_multi[206:198])
+	);
+
+	modulo_multiplicador inst_multi24(
+     .a(matriz_A[214:207]),
+     .b(matriz_B[214:207]),
+     .resultado(matriz_multi[215:207])
+	);
+
+	modulo_multiplicador inst_multi25(
+     .a(matriz_A[223:216]),
+     .b(matriz_B[223:216]),
+     .resultado(matriz_multi[224:216])
+	);
 
 
 	
